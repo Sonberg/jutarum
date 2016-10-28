ea447d0dd261e29db676d07504c471e0ce66e76c
Sonberg <per.sonberg@gmail.com>
2016-10-28 23:53:31 +0200

    loop bug fixed

--- app/components/user-missions/component.js
+++ app/components/user-missions/component.js
@@ -17,35 +17,42 @@ export default Ember.Component.extend({
     var missions = self.get("missions");
     var rapports = self.get("rapports");
 
-    if (missions.length > 0) {
+    if (missions.length > 0 && rapports !== null) {
       self.set("new", Ember.A());
       self.set("old", Ember.A());
-      self.get("missions").forEach(function(mission) {
-        if (rapports) {
-          self.get("inRapport")(rapports, mission, self);
-        } else {
-          self.get("new").pushObject(mission);
+      self.get("missions").every(function(mission) {
+        console.log("miss");
+        if (self.get("new.length") + self.get("old.length") <= self.get("missions.length")) {
+          if (rapports) {
+            console.log("got rapport");
+            self.get("inRapport")(rapports, mission, self);
+          } else {
+            self.get("new").pushObject(mission);
+          }
+          
+          if (self.get("reloadBtn")) {
+            self.get("reloadBtn").stop();
+          }
         }
         
-        if (self.get("reloadBtn")) {
-          self.get("reloadBtn").stop();
-        }
+        return true;
       });
     }
     
-  }.observes("missions.[]", "rapports.[]").on("init"),
+  }.observes("missions.[]").on("init"),
   
   _refresh: function() {
     this.get("setup")();
   },
 
   rapports: Ember.computed('model.rapports.@each', function() {
-    var model = this.get('model.rapports');
-      var arr = model.filter(function(item, index, enumerable) {
+
+      var arr = this.get('store').peekAll("rapport").filter(function(item, index, enumerable) {
         if (item.data["mission-id"] !== null) {
           return true;
         }
       }).map(function(rapport, index, enumerable) {
+        console.log(rapport);
         return {
           id: rapport.get("id"),
           team: rapport.get("team"),
@@ -67,24 +74,30 @@ export default Ember.Component.extend({
 
   inRapport: function(rapports, mission, self) {
     var id = parseInt(mission.get("id"));
-    for (var i = 0; i < rapports.length; i++) {
-      var rapport = rapports[i];
-      var missionId = parseInt(rapport["mission-id"]);
-      
-      if (id === missionId) {
-        var team = JSON.parse(rapport["team"]);
-        if (team.length > 0) {
-          for (var i = 0; i < team.length; i++) {
-            var user = parseInt(team[i].id);
-            var me = parseInt(self.cookie.getCookie('user'));
-            
-            if (me === user) {
-              self.get("old").pushObject(mission);
-              return true;
+    if (Object.prototype.toString.call(rapports) === '[object Array]' ) {
+      for (var i = 0; i < rapports.length; i++) {
+        var rapport = rapports[i];
+        var missionId = parseInt(rapport["mission-id"]);
+        
+        if (id === missionId) {
+          var team = JSON.parse(rapport["team"]);
+          if (Object.prototype.toString.call(team) === '[object Array]' ) {
+            for (var i = 0; i < team.length; i++) {
+              console.log("team");
+              var user = parseInt(team[i].id);
+              var me = parseInt(self.cookie.getCookie('user'));
+              
+              if (me === user) {
+                self.get("old").pushObject(mission);
+                return true;
+              }
             }
+            self.get("new").pushObject(mission);
+            return true;
+            console.log("is array");
           }
-          console.log("is array");
         }
+        console.log("rapport");
       }
     }
     self.get("new").pushObject(mission);
